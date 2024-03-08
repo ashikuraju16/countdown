cb94803bb5dc840c27554257cbc1cd67c68bd84a
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-03-08 21:48:48 +0600

    js

--- script/s-s-r.js
+++ script/s-s-r.js
@@ -1,8 +1,12 @@
-let countNumbers;
+
 let numbers = 0;
 
-function startCount(){
-document.getElementById('start-btn').disabled = true;
-document.getElementById('stop-btn').disabled = false;
-countNumbers.
+function startCount() {
+  document.getElementById("start-btn");
+  const startNumbers = document.getElementById("count-numbers");
+
+  startNumbers = setInterval(function () {
+    startNumbers.innerHTML = numbers;
+    numbers++;
+  }, 1000);
 }
