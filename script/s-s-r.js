7b6dd545507ce85e813c1ce2b973638c704de999
ashikuraju16 <ashikur.r.raju@gmail.com>
2024-03-08 21:33:59 +0600

    body

---
+++ script/s-s-r.js
@@ -0,0 +1,8 @@
+let countNumbers;
+let numbers = 0;
+
+function startCount(){
+document.getElementById('start-btn').disabled = true;
+document.getElementById('stop-btn').disabled = false;
+countNumbers.
+}
